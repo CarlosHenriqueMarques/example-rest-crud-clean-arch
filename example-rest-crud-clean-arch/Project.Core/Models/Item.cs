38038650c09d5790e7a31ed72a828f8861c684bb
Carlos Marques <carlos.marques89@gmail.com>
2023-07-20 15:34:48 -0400

    Implementing repository

---
+++ example-rest-crud-clean-arch/Project.Core/Models/Item.cs
@@ -0,0 +1,8 @@
+﻿namespace example_rest_crud_clean_arch.Project.Core.Models;
+
+public class Item
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public decimal Value { get; set; }
+}
---
+++ example-rest-crud-clean-arch/Project.Infrastructure/Data/ItemRepository.cs
@@ -0,0 +1,41 @@
+﻿using example_rest_crud_clean_arch.Project.Core.Interfaces;
+using example_rest_crud_clean_arch.Project.Core.Models;
+
+namespace example_rest_crud_clean_arch.Project.Infrastructure.Data;
+
+public class ItemRepository : IItemRepository
+{
+    private List<Item> _items = new();
+    public void AddItem(Item item)
+    {
+        //Dump way to do it in memory database
+        item.Id = _items.Count +1;
+        _items.Add(item);
+    }
+
+    public void DeleteItem(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<Item> GetAllItems()
+    {
+        return _items;
+    }
+
+    public Item GetItemById(int id)
+    {
+        return _items.FirstOrDefault(item => item.Id == id);
+    }
+
+    public void UpdateItem(Item item)
+    {
+        var existItem = _items.FirstOrDefault(i => i.Id == item.Id);
+        if (existItem == null) 
+        {
+            throw new InvalidOperationException("Id invalid!");
+        }
+        existItem.Name = item.Name;
+        existItem.Value = item.Value;
+    }
+}
